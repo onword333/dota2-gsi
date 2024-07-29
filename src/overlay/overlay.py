5aa8677ca045592201e17181d0cb64376cb549df
Klepov Yuri <onword333@gmail.com>
2024-07-29 22:00:48 +0300

    Добавлено дрожание окна
    За 10 секунд до появления руны окно будет дрожать

--- src/overlay/overlay.py
+++ src/overlay/overlay.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import requests
 from runes.gold_rune import GoldRune
 from runes.wisdom_rune import WisdomRune
+import random
 
 
 class OverlayApp(tk.Tk):
@@ -24,16 +25,52 @@ class OverlayApp(tk.Tk):
         self.label = tk.Label(self, text="Waiting for data...", fg="white", bg="black")
         self.label.pack(pady=20)
 
+        self.shaking = False
+
+        self.after(100, self.initialize_position)        
+
+    def shake(self):
+        end_time = self.after(1000, self.stop_shake)
+        self.perform_shake(end_time)
+
+    def perform_shake(self, end_time):
+        if not self.shaking:
+            return
+
+        x_offset = random.randint(-5, 5)
+        y_offset = random.randint(-5, 5)
+        self.geometry(f"+{self.original_x + x_offset}+{self.original_y + y_offset}")
+        self.after(50, self.perform_shake, end_time)
+
+    def stop_shake(self):
+        self.shaking = False
+        self.geometry(f"+{self.original_x}+{self.original_y}")
+
+    def initialize_position(self):
+        self.update_idletasks()
+        self.original_x = self.winfo_x()
+        self.original_y = self.winfo_y()
+
         self.update_data()
 
     def update_data(self):
-        try:            
+        try:
             response = requests.get('http://localhost:3000/get_data')
             data = response.json()
 
+            rune_coming_soon = False
+
             msg = ''
-            for rune in self.runes:                
-                msg += f'{rune.name}: {rune.time_until_next_appearance(data)}\n'
+            for rune in self.runes:
+                time_until_next_appearance = rune.time_until_next_appearance(data)
+                msg += f'{rune.name}: {time_until_next_appearance}\n'
+
+                if time_until_next_appearance <= 10 and time_until_next_appearance != 0:
+                    rune_coming_soon = True
+
+            if rune_coming_soon and not self.shaking:
+                self.shaking = True
+                self.shake()
 
             self.label.config(text=msg)
         except Exception as e:
